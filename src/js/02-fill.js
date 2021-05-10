f41c8582ec858c54a29590cc123acc81b3653f90
Laura733 <lali733@gmail.com>
2021-05-10 16:29:10 +0200

    Adding Chance filljs

--- src/js/02-fill.js
+++ src/js/02-fill.js
@@ -1,6 +1,22 @@
 'user strict';
 
-const fillFullName = document.querySelector(".js-fillFullName");
-const fillJob = document.querySelector(".js-fillJob");
+const fillForm = document.querySelector(".js-fillform");
+const fillFullName = document.querySelector(".js-fullName");
+/*const fillJob = document.querySelector(".js-fillJob");*/
 const photoFullName = document.querySelector(".js-photoFullName");
-const photoJob = document.querySelector(".js-photoJob")
+/*const photoJob = document.querySelector(".js-photoJob")*/
+
+function changeName(ev) {
+    const fillFullNameChange = ev.target;
+    const fillFullNameValue = fillFullNameChange.value;
+
+    if(fillFullNameChange.name === 'full-name') {
+        photoFullName.innerHTML = fillFullNameValue;
+    }
+    console.log('target', ev.target);
+    console.log('current target', ev.currentTarget);
+}
+
+//fillForm.addEventListener(changeName);
+//fillForm.addEventListener('change', changeName);
+/*x.addEventListener('change', changeJob)*/
