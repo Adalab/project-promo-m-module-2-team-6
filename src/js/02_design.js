8a9f96196921ce6fcc5c6972132b06bc3a20faa8
Rocío Garrido <rocioganon@gmail.com>
2021-05-14 19:20:24 +0200

    add palette

--- src/js/02_design.js
+++ src/js/02_design.js
@@ -1,11 +1,25 @@
 'use strict';
-/*
-const colorPallete = document.querySelector('.js-colorPallete1');
 
-function oncolorPalleteClick(){
-    const selectColor = colorPallete.nodeValue;
-    if(selectColor === line1){}
+const buttons = document.querySelectorAll ('.js-palette');
+
+function changeColor() {
+  const selectedButton = document.querySelector('.js-palette:checked');
+  const card = document.querySelector('.js-card');
+  card.classList.remove('js-color-blue');
+  card.classList.remove('js-color-red');
+
+  // eslint-disable-next-line no-empty
+  if (selectedButton.value === 'green') {
+  } else if (selectedButton.value === 'red') {
+    card.classList.add('js-color-red');
+  } else {
+    card.classList.add('js-color-blue');
+  }
 }
 
+changeColor();
 
-colorPallete.addEventListener('click', oncolorPalleteClick)*/
+for (let index = 0; index < buttons.length; index++) {
+  const button = buttons[index];
+  button.addEventListener('click', changeColor);
+}
