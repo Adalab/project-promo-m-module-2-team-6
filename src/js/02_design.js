395dbc940fce5d327c0edbcd7fa1ae43d110fda3
Laura733 <lali733@gmail.com>
2021-05-16 00:16:21 +0200

    Trying js Design

--- src/js/02_design.js
+++ src/js/02_design.js
@@ -5,14 +5,18 @@ const colorPallete2 = document.querySelector(".js-colorPallete2");
 const colorPallete3 = document.querySelector(".js-colorPallete3");
 //console.log(colorPallete1);
 
-const colorPallete = {};
+const colorPallete = {
+    colorPallete1:'',
+    colorPallete2:'',
+    colorPallete3:''
+}
 
 function clickPallete(){
 for ( let i=0; i < colorPallete.length; i++) {
 
 };
 }
-/*
+
 const colorPallete = document.querySelector('.js-colorPallete1');
 
 function oncolorPalleteClick(){
--- src/js/03_fill.js
+++ src/js/03_fill.js
@@ -3,10 +3,10 @@
 const fillForm = document.querySelector('.js-fillform');
 const photoFullName = document.querySelector('.js-photoFullName');
 const photoJob = document.querySelector('.js-photoJob');
-//const photoEmail = document.querySelector('.js-photoEmail');
-//const photoLinkedin = document.querySelector('.js-photoLinkedin');
-//const photoGitHub = document.querySelector('.js-photoGithub');
-//const photoPhone = document.querySelector('.js-photoPhone') ;
+const photoEmail = document.querySelector('.js-photoEmail');
+const photoLinkedin = document.querySelector('.js-photoLinkedin');
+const photoGitHub = document.querySelector('.js-photoGithub');
+const photoPhone = document.querySelector('.js-photoPhone') ;
 
 const formElement = {
   name:'',
@@ -20,12 +20,15 @@ function newFormElements(ev) {
   const element = ev.target;
   const value = element.value;
   formElement[element] = value;
-  console.log(formElement);
 }
 
 function newPhotoElements() {
   photoFullName.innerText = formElement.name;
   photoJob.innerText = formElement.job;
+  photoEmail.href = formElement.email;
+  photoLinkedin.href = formElement.linkedin;
+  photoGitHub.href = formElement.github;
+  photoPhone.href = formElement.phone;
 }
 
 /*function onFormKeyUp(ev) {
