d220f66871ef5837e21e8984eb17b7d7f4e02584
Rocío Garrido <rocioganon@gmail.com>
2021-05-15 16:10:11 +0200

    redo function palette

--- src/js/02_design.js
+++ src/js/02_design.js
@@ -5,21 +5,20 @@ const buttons = document.querySelectorAll ('.js-palette');
 function changeColor() {
   const selectedButton = document.querySelector('.js-palette:checked');
   const card = document.querySelector('.js-card');
-  card.classList.remove('js-color-blue');
-  card.classList.remove('js-color-red');
 
-  // eslint-disable-next-line no-empty
   if (selectedButton.value === 'green') {
+    card.classList.remove('js-color-blue');
+    card.classList.remove('js-color-red');
   } else if (selectedButton.value === 'red') {
     card.classList.add('js-color-red');
-  } else {
+  } else if (selectedButton.value === 'blue') {
     card.classList.add('js-color-blue');
   }
 }
 
 changeColor();
 
-for (let index = 0; index < buttons.length; index++) {
-  const button = buttons[index];
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i];
   button.addEventListener('click', changeColor);
 }
