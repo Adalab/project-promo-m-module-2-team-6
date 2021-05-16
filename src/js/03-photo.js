98695362acf7bc5c6c3c8005e8d1977d065a6209
Rocío Garrido <rocioganon@gmail.com>
2021-05-16 19:16:50 +0200

    comment photo.js

--- src/js/03-photo.js
+++ src/js/03-photo.js
@@ -1,8 +1,13 @@
 'use strict';
 
+// Componente de foto de perfil. Para que funcione, necesitamos estos elementos:
+// Campo de tipo "file"
 const fr = new FileReader();
+// Botón: 
 const fileField = document.querySelector('.js__profile-upload-btn');
+// Contenedor principal de la imagen:
 const profileImage = document.querySelector('.js__profile-image');
+// Contenedor de la previsualización de la imagen:
 const profilePreview = document.querySelector('.js__profile-preview');
 
 function getImage(e){
@@ -11,15 +16,13 @@ function getImage(e){
   fr.readAsDataURL(myFile);
 }
 
-
 function writeImage() {
   profileImage.style.backgroundImage = `url(${fr.result})`;
   profilePreview.style.backgroundImage = `url(${fr.result})`;
 }
 
-
-function fakeFileClick() {
-  fileField.click(); 
-}
+/*function fakeFileClick() {
+  fileField.click();
+}*/
 
 fileField.addEventListener('change', getImage);
