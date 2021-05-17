e5db8f465e51b9977fa2063bb896bfda3177f680
Miriam-Villalta <[email address]mvb_8@hotmail.com>
2021-05-17 13:14:22 +0200

    arrows

--- src/js/00-collapsible.js
+++ src/js/00-collapsible.js
@@ -2,6 +2,8 @@
 
 const collapsibleHeaders = document.querySelectorAll('.js-collapsible-header');
 const collapsibleBox = document.querySelectorAll('.js-collapsible');
+//array de las flechas
+const arrow = document.querySelectorAll('.js-arrow');
 for (const collapsibleHeader of collapsibleHeaders) {
   collapsibleHeader.addEventListener('click', switchCollapsible);
 }
@@ -13,7 +15,15 @@ function switchCollapsible(ev) {
     '.js-collapsible');
   for (let i=0; i< allCollapsibles.length; i++) {
     if(clickedHeader === collapsibleHeaders[i]) {
+      console.log(collapsibleBox[i]);
+      arrow[i].classList.remove('fa-chevron-down');
+      arrow[i].classList.add('fa-chevron-up');
       collapsibleBox[i].classList.toggle('collapsible--close');
-    }else { collapsibleBox[i].classList.add('collapsible--close'); }
+    }else { collapsibleBox[i].classList.add('collapsible--close'); 
+      
+      arrow[i].classList.add('fa-chevron-down');
+      
+      }
+      console.log(arrow[i]);
   }
 }
