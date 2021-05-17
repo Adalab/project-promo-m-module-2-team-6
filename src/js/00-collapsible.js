d2b91183610e82a3683f7c9d6c8a9236d24b58ae
Yosiris Mariñez <yosiris0510@gmail.com>
2021-05-17 14:13:36 +0200

    changes styles

--- src/js/00-collapsible.js
+++ src/js/00-collapsible.js
@@ -1,19 +1,20 @@
-'use strict';
+"use strict";
 
-const collapsibleHeaders = document.querySelectorAll('.js-collapsible-header');
-const collapsibleBox = document.querySelectorAll('.js-collapsible');
+const collapsibleHeaders = document.querySelectorAll(".js-collapsible-header");
+const collapsibleBox = document.querySelectorAll(".js-collapsible");
 for (const collapsibleHeader of collapsibleHeaders) {
-  collapsibleHeader.addEventListener('click', switchCollapsible);
+  collapsibleHeader.addEventListener("click", switchCollapsible);
 }
 
 function switchCollapsible(ev) {
   const clickedHeader = ev.currentTarget;
 
-  const allCollapsibles = document.querySelectorAll(
-    '.js-collapsible');
-  for (let i=0; i< allCollapsibles.length; i++) {
-    if(clickedHeader === collapsibleHeaders[i]) {
-      collapsibleBox[i].classList.toggle('collapsible--close');
-    }else { collapsibleBox[i].classList.add('collapsible--close'); }
+  const allCollapsibles = document.querySelectorAll(".js-collapsible");
+  for (let i = 0; i < allCollapsibles.length; i++) {
+    if (clickedHeader === collapsibleHeaders[i]) {
+      collapsibleBox[i].classList.toggle("collapsible--close");
+    } else {
+      collapsibleBox[i].classList.add("collapsible--close");
+    }
   }
 }
