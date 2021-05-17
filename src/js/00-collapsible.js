04b8d324c193532cfd3e659b5f8ca22ff18a2a94
Laura733 <lali733@gmail.com>
2021-05-17 13:51:25 +0200

    Adding arrow Js

--- src/js/00-collapsible.js
+++ src/js/00-collapsible.js
@@ -15,15 +15,13 @@ function switchCollapsible(ev) {
     '.js-collapsible');
   for (let i=0; i< allCollapsibles.length; i++) {
     if(clickedHeader === collapsibleHeaders[i]) {
-      console.log(collapsibleBox[i]);
-      arrow[i].classList.remove('fa-chevron-down');
-      arrow[i].classList.add('fa-chevron-up');
+      arrow[i].classList.toggle('fa-chevron-up');
       collapsibleBox[i].classList.toggle('collapsible--close');
-    }else { collapsibleBox[i].classList.add('collapsible--close'); 
-      
+    }else { 
+      collapsibleBox[i].classList.add('collapsible--close');
+      arrow[i].classList.remove('fa-chevron-up');
       arrow[i].classList.add('fa-chevron-down');
-      
-      }
       console.log(arrow[i]);
+    }
   }
 }
