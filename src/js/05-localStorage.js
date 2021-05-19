c3370c2eab28bedade63014a5d27684073e9fc9a
Rocío Garrido <rocioganon@gmail.com>
2021-05-19 19:16:48 +0200

    add explanation to localStorage

--- src/js/05-localStorage.js
+++ src/js/05-localStorage.js
@@ -18,33 +18,32 @@ function saveInLocalStorage() {
   localStorage.setItem("userData", userDataInString);
 }
 
-// al arrancar la página recogemos los datos desde el local storage y actualizamos el formulario
+// Al arrancar la página recogemos los datos desde el local storage y actualizamos el formulario
 function getFromLocalStorage() {
   // obtengo los datos desde el local storage
   const userDataInString = localStorage.getItem("userData");
   // compruebo si hay datos válidos, es decir si la usuaria ya había entrado en nuestra web anteriormente
   if (userDataInString !== null) {
     const userData = JSON.parse(userDataInString);
-    // actualizo los inputs del formulario
+    // Actualizo los inputs del formulario
     document.querySelector(".js-input-name").value = userData.name;
     document.querySelector(".js-input-job").value = userData.job;
     document.querySelector(".js-input-email").value = userData.email;
     document.querySelector(".js-input-phone").value = userData.phone;
     document.querySelector(".js-input-linkedin").value = userData.linkedin;
     document.querySelector(".js-input-github").value = userData.github;
-    // actualizo la variable global de la foto
+    // Actualizo la variable global de la foto
     photo = userData.photo;
-    // actualizo la paleta, para ello
-    // - Recorro las 3 paletas
-    // - Compruebo cuál tiene el valor que tengo en el local storage
-    // - A la paleta que tiene el valor correcto le hago un .checked = true para activarla
+
+    // Actualizo la paleta: recorro las 3 paletas, compruebo cuál tiene el valor que tengo en el local storage, a la paleta que tiene el valor correcto le hago un .checked = true para activarla
     const paletteElements = document.querySelectorAll(".js-palette");
     for (const paletteElement of paletteElements) {
       if (paletteElement.value === userData.palette) {
         paletteElement.checked = true;
       }
     }
-    // propago los datos desde el formulario a la tarjeta
+
+    // Propago los datos desde el formulario a la tarjeta
     updateAllInputs();
     handlePalette();
     updatePhoto();
